b4e794a1bfd2444197e692969e7e9d7d477a4e80
Joshua Paul Chan <joshpaulchan@gmail.com>
2020-11-21 11:16:44 -0500

    use random + cmp + matcher to print act on it

--- main.rs
+++ main.rs
@@ -1,9 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your gress.");
+    let target = rand::thread_rng().gen_range(1, 101);
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
@@ -11,5 +15,12 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You wrote: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    match target.cmp(&guess) {
+        Ordering::Less => println!("guess was too high!"),
+        Ordering::Greater => println!("guess was too low!"),
+        Ordering::Equal => println!("right on target!"),
+    }
+
+    println!("You wrote: {}, target was {}", guess, target);
 }
