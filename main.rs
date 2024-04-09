738630ed8be3a1f3dcb259db4177e5b07c5deb9f
Joshua Paul Chan <joshpaulchan@gmail.com>
2024-04-08 22:35:34 -0400

    checkpoint

--- main.rs
+++ main.rs
@@ -1,15 +1,19 @@
+use juniper::{EmptyMutation, RootNode};
 use std::env;
-use warp::Filter;
+use warp::{Filter};
 
 #[tokio::main]
 async fn main() {
+    let log = warp::log("server");
+
     let port: u16 = env::var("PORT")
         .map(|val| val.parse().unwrap())
         .unwrap_or(3030);
-    // let port: u16 = 3030;
+
+    log::info!("Listening on 0.0.0.0:{}", port);
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let routes = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let routes = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name)).with(log);
     // Match any request and return hello world!
     // let routes = warp::any().map(|| "Hello, World!");
 
